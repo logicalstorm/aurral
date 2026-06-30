b325205b4a1f7642fd77983a52a2965c8be5514f
lklynet <hello@leekelly.org>
2026-06-30 10:15:21 -0400

    Integrate @fullhuman/postcss-purgecss for production CSS optimization and update postcss.config.js. Clean up index.css by removing unused styles and improving overall structure.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,16 @@
-export default {
-  plugins: {
-    autoprefixer: {},
-  },
+import purgeCSSPlugin from '@fullhuman/postcss-purgecss';
+
+const plugins = {
+  autoprefixer: {},
 };
+
+if (process.env.NODE_ENV === 'production') {
+  plugins['@fullhuman/postcss-purgecss'] = purgeCSSPlugin({
+    content: ['./src/**/*.{jsx,js}'],
+    safelist: {
+      standard: [/^is-/],
+    },
+  });
+}
+
+export default { plugins };
